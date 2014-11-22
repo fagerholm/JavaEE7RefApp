86dbd0518c0135d3608608ccdee30047db539a63
Jon André Fagerholm <jon.fagerholm@gmail.com>
2014-11-22 15:17:52 +0100

    Fixed violations

--- src/main/java/no/jaf/jee7/refapp/websocket/endpoint/EchoEndpoint.java
+++ src/main/java/no/jaf/jee7/refapp/websocket/endpoint/EchoEndpoint.java
@@ -5,7 +5,6 @@ import javax.websocket.OnOpen;
 import javax.websocket.server.ServerEndpoint;
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
-import java.io.IOException;
 
 @ServerEndpoint("/echo")
 public class EchoEndpoint {
@@ -15,18 +14,15 @@ public class EchoEndpoint {
     @OnOpen
     public void connect(Session session) {
         this.session = session;
-        System.out.println(session);
     }
 
     @OnClose
     public void close() {
         this.session = null;
-        System.out.println("Closed");
     }
 
     @OnMessage
     public void onMessage(String msg) {
-        System.out.println("Msg: " + msg);
         session.getAsyncRemote().sendText("Echo: " + msg);
     }
 }
